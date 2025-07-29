109e6e95e4952742696b7a9ccbe0dcbfdd7784ff
vikram_bn <vikram@reachmobile.com>
2025-07-29 19:05:54 +0530

    added google analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+// Google Analytics TypeScript declarations
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+  
+  function gtag(...args: any[]): void;
+}
+
+export {};
